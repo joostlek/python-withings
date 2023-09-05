e8b18cefe866b4ae184221e608124ac814f93d17
Joostlek <joostlek@outlook.com>
2023-09-05 17:24:40 +0200

    Add method to fetch goals

--- src/aiowithings/util.py
+++ src/aiowithings/util.py
@@ -1,7 +1,7 @@
 """Asynchronous Python client for Withings."""
 from __future__ import annotations
 
-from typing import Any, TypeVar
+from typing import Any, TypeVar, cast
 
 from aiowithings.const import LOGGER
 
@@ -23,3 +23,15 @@ def to_enum(
             str(enum_class),
         )
         return default_value
+
+
+def get_measurement(value: int, unit: int) -> float:
+    """Convert value and unit to real value."""
+    return cast(float, value * pow(10, unit))
+
+
+def get_measurement_from_dict(input_dict: dict[str, Any]) -> float:
+    """Convert dict with value and unit to real value."""
+    value = cast(int, input_dict["value"])
+    unit = cast(int, input_dict["unit"])
+    return get_measurement(value, unit)
---
+++ tests/test_util.py
@@ -0,0 +1,12 @@
+"""Asynchronous Python client for Withings."""
+from aiowithings.util import get_measurement, get_measurement_from_dict
+
+
+def test_measurement() -> None:
+    """Test measurement."""
+    assert get_measurement(20, -1) == 2
+
+
+def test_measurement_from_dict() -> None:
+    """Test measurement."""
+    assert get_measurement_from_dict({"value": 20, "unit": -1}) == 2

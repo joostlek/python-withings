4fc17af96fd63475e17788e8c2765875eed9386f
Joost Lekkerkerker <joostlek@outlook.com>
2023-10-11 13:46:28 +0200

    Add method to aggregate sleep summary (#52)

--- src/aiowithings/helpers.py
+++ src/aiowithings/helpers.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from aiowithings.models import SleepSummary
+
 if TYPE_CHECKING:
     from aiowithings import MeasurementGroup, MeasurementType
 
@@ -18,3 +20,148 @@ def aggregate_measurements(
             result[data_point.measurement_type] = data_point.value
 
     return result
+
+
+def aggregate_sleep_summary(
+    sleep_summaries: list[SleepSummary],
+) -> SleepSummary | None:
+    """Aggregate the sleep summaries to return a combined report."""
+    if len(sleep_summaries) == 0:
+        return None
+    if len(sleep_summaries) == 1:
+        return sleep_summaries[0]
+    sorted_sleep_summaries = sorted(
+        sleep_summaries,
+        key=lambda sleep_summary: sleep_summary.start_date,
+    )
+
+    def average(data: list[int]) -> int:
+        return round(sum(data) / len(data))
+
+    return SleepSummary(
+        start_date=sorted_sleep_summaries[0].start_date,
+        end_date=sorted_sleep_summaries[1].end_date,
+        date=sorted_sleep_summaries[0].date,
+        hashed_device_id=sorted_sleep_summaries[0].hashed_device_id,
+        breathing_disturbances_intensity=average(
+            [
+                summary.breathing_disturbances_intensity
+                for summary in sleep_summaries
+                if summary.breathing_disturbances_intensity is not None
+            ],
+        ),
+        deep_sleep_duration=sum(
+            summary.deep_sleep_duration
+            for summary in sleep_summaries
+            if summary.deep_sleep_duration is not None
+        ),
+        sleep_latency=average(
+            [
+                summary.sleep_latency
+                for summary in sleep_summaries
+                if summary.sleep_latency is not None
+            ],
+        ),
+        wake_up_latency=average(
+            [
+                summary.wake_up_latency
+                for summary in sleep_summaries
+                if summary.wake_up_latency is not None
+            ],
+        ),
+        average_heart_rate=average(
+            [
+                summary.average_heart_rate
+                for summary in sleep_summaries
+                if summary.average_heart_rate is not None
+            ],
+        ),
+        max_heart_rate=average(
+            [
+                summary.max_heart_rate
+                for summary in sleep_summaries
+                if summary.max_heart_rate is not None
+            ],
+        ),
+        min_heart_rate=average(
+            [
+                summary.min_heart_rate
+                for summary in sleep_summaries
+                if summary.min_heart_rate is not None
+            ],
+        ),
+        light_sleep_duration=sum(
+            summary.light_sleep_duration
+            for summary in sleep_summaries
+            if summary.light_sleep_duration is not None
+        ),
+        rem_sleep_duration=sum(
+            summary.rem_sleep_duration
+            for summary in sleep_summaries
+            if summary.rem_sleep_duration is not None
+        ),
+        average_respiration_rate=average(
+            [
+                summary.average_respiration_rate
+                for summary in sleep_summaries
+                if summary.average_respiration_rate is not None
+            ],
+        ),
+        max_respiration_rate=average(
+            [
+                summary.max_respiration_rate
+                for summary in sleep_summaries
+                if summary.max_respiration_rate is not None
+            ],
+        ),
+        min_respiration_rate=average(
+            [
+                summary.min_respiration_rate
+                for summary in sleep_summaries
+                if summary.min_respiration_rate is not None
+            ],
+        ),
+        sleep_score=max(
+            (
+                summary.sleep_score
+                for summary in sleep_summaries
+                if summary.sleep_score is not None
+            ),
+            default=0,
+        ),
+        snoring=average(
+            [
+                summary.snoring
+                for summary in sleep_summaries
+                if summary.snoring is not None
+            ],
+        ),
+        snoring_count=sum(
+            summary.snoring_count
+            for summary in sleep_summaries
+            if summary.snoring_count is not None
+        ),
+        wake_up_count=sum(
+            summary.wake_up_count
+            for summary in sleep_summaries
+            if summary.wake_up_count is not None
+        ),
+        total_time_awake=average(
+            [
+                summary.total_time_awake
+                for summary in sleep_summaries
+                if summary.total_time_awake is not None
+            ],
+        ),
+        active_movement_duration=None,
+        apnea_hypopnea_index=None,
+        average_movement_score=None,
+        external_time_asleep=None,
+        out_of_bed_count=None,
+        rem_sleep_phase_count=None,
+        sleep_efficiency=None,
+        time_awake_during_sleep=None,
+        total_sleep_time=None,
+        total_time_in_bed=None,
+        withings_index=None,
+    )
--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -4,7 +4,8 @@ from __future__ import annotations
 import json
 from typing import TYPE_CHECKING, Any
 
-from aiowithings import MeasurementGroup, aggregate_measurements
+from aiowithings import MeasurementGroup, SleepSummary, aggregate_measurements
+from aiowithings.helpers import aggregate_sleep_summary
 
 from . import load_fixture
 
@@ -19,3 +20,32 @@ def test_aggregate_measurements(snapshot: SnapshotAssertion) -> None:
     measurements = [MeasurementGroup.from_api(measurement) for measurement in json_file]
 
     assert aggregate_measurements(measurements) == snapshot
+
+
+def test_aggregate_sleep_summary(snapshot: SnapshotAssertion) -> None:
+    """Test aggregation."""
+    json_file: list[dict[str, Any]] = json.loads(load_fixture("sleep_summary.json"))[
+        "body"
+    ]["series"]
+
+    sleep_summaries = [
+        SleepSummary.from_api(sleep_summary) for sleep_summary in json_file
+    ]
+
+    assert aggregate_sleep_summary(sleep_summaries) == snapshot
+
+
+def test_aggregate_single_sleep_summary(snapshot: SnapshotAssertion) -> None:
+    """Test aggregation."""
+    json_file: dict[str, Any] = json.loads(load_fixture("sleep_summary.json"))["body"][
+        "series"
+    ][0]
+
+    sleep_summary = SleepSummary.from_api(json_file)
+
+    assert aggregate_sleep_summary([sleep_summary]) == snapshot
+
+
+def test_aggregate_no_sleep_summary(snapshot: SnapshotAssertion) -> None:
+    """Test aggregation."""
+    assert aggregate_sleep_summary([]) == snapshot

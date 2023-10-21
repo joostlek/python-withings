eb4ea7128afba3727eb00c77638c175e941b4e1b
Joostlek <joostlek@outlook.com>
2023-10-21 22:14:10 +0200

    Add workout models

--- src/aiowithings/helpers.py
+++ src/aiowithings/helpers.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from aiowithings.models import (
-    MeasurementGroupAttribution,
+    MeasurementAttribution,
     MeasurementType,
     SleepSummary,
 )
@@ -23,8 +23,8 @@ def aggregate_measurements(
 
     for measurement in groups:
         if measurement.attribution not in (
-            MeasurementGroupAttribution.UNKNOWN,
-            MeasurementGroupAttribution.DEVICE_ENTRY_FOR_USER_AMBIGUOUS,
+                MeasurementAttribution.UNKNOWN,
+                MeasurementAttribution.DEVICE_ENTRY_FOR_USER_AMBIGUOUS,
         ):
             for data_point in measurement.measurements:
                 result[data_point.measurement_type] = data_point.value

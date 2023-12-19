ece6ab855c3ff1bca5224ed720e93ca362d630ed
Joost Lekkerkerker <joostlek@outlook.com>
2023-12-19 15:43:48 +0100

    Improve tests (#132)

---
+++ tests/const.py
@@ -0,0 +1,12 @@
+"""Constants for tests."""
+from importlib import metadata
+
+WITHINGS_URL = "https://wbsapi.withings.net:443"
+
+version = metadata.version("aiowithings")
+
+HEADERS = {
+    "User-Agent": f"AioWithings/{version}",
+    "Accept": "application/json, text/plain, */*",
+    "Authorization": "Bearer test",
+}
